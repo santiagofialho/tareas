7206a57caed5f9dce7a553dd76888031af9d6b05
santiagofialho <167836783+santiagofialho@users.noreply.github.com>
2024-04-23 01:50:26 -0300

    Add files via upload

---
+++ largo y ancho.py
@@ -0,0 +1,15 @@
+num1 = 45
+num2 = 34
+resultado = num1 + num2
+
+print("El resultado de la suma es:", resultado)
+ancho = 45
+largo = 34
+
+area = ancho * largo
+texto_ancho = "El ancho del rectángulo es: "
+texto_largo = "El largo del rectángulo es: "
+texto_area = "El área del rectángulo es: "
+print(texto_ancho + str(ancho))
+print(texto_largo + str(largo))
+print(texto_area + str(area))
---
+++ suma.py
@@ -0,0 +1,6 @@
+num1 = 90
+num2 = 35
+resultado = num1 + num2
+
+print("El resultado de la suma es:", resultado)
+
